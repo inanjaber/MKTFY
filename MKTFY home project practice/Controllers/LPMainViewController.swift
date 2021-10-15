b921dc854196b7da6fc2a9460812336b001facfd
Inan <inan73@Inans-MacBook-Pro.local>
2021-10-15 13:53:37 -0600

    first commit

---
+++ MKTFY home project practice/Controllers/LPMainViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LPMainViewController.swift
+//  MKTFY home project practice
+//
+//  Created by Inan Jaber on 2021-10-15.
+//
+
+import UIKit
+
+class LPMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MKTFY home project practice/Controllers/LPRootViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LPRootViewController.swift
+//  MKTFY home project practice
+//
+//  Created by Inan Jaber on 2021-10-15.
+//
+
+import UIKit
+
+class LPRootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
